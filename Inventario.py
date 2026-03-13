524ef577ed670661e201840d7cf87aa97e027d97
Joan <estremorjoan@gmail.com>
2026-03-13 18:55:15 -0500

    readme

--- Inventario.py
+++ Inventario.py
@@ -10,4 +10,18 @@ while True:
     except:
         print("ERROR: ingrese el precio correctamente ")
 
+## Funcion de cantidad
+while True:
 
+    try:
+        cantidad = int(input("Ingrece la cantidad deseada: "))
+        break
+    except:
+        print("ERROR: ingrese la cantidad correctamente ")
+
+## Funcion de costo total   
+costo_total = precio * cantidad
+
+## Resumen de los productos
+print("\n === RESIUMEN DEL PRODUCTO ===\n")
+print(f"Producto: {nombre} | Precio: {precio} | Cantidad: {cantidad} | Total: {costo_total} ") 
